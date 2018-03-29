227714832ca3d9d3cc5857de412b155d5b19d34d
Yohei <yo_kubocchi@hotmail.com>
2018-03-29 00:10:31 -0700

    New

---
+++ CS_P036_Exercise/CS_P036_Exercise/Calculation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_P036_Exercise
+{
+    class Calculation
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public int Length { get; set; }
+
+        public int Volume()
+        {
+            int volume;
+
+            volume = Height * Width * Length;
+
+            return volume;
+        }
+
+
+    }
+}
---
+++ CS_P036_Exercise/CS_P036_Exercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_P036_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rectangular volume calculation.\nType Height, Width and Length with inch.");
+            Calculation calculation = new Calculation();
+            calculation.Height = Convert.ToInt32(Console.ReadLine());
+            calculation.Width = Convert.ToInt32(Console.ReadLine());
+            calculation.Length = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The volume of this rectangular is: " + calculation.Volume() + "cubic inches");
+            Console.Read();
+        }
+    }
+}
