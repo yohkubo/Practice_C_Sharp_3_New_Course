9338ebe8b7c72e26eca15121271ae0bff35e62ba
Yohei <yo_kubocchi@hotmail.com>
2018-03-28 20:25:10 -0700

    New

--- CS_P033_ClassesAndObjects/CS_P033_ClassesAndObjects/Program.cs
+++ CS_P033_ClassesAndObjects/CS_P033_ClassesAndObjects/Program.cs
@@ -11,18 +11,30 @@ namespace CS_P033_ClassesAndObjects
         static void Main(string[] args)
         {
             Deck deck = new Deck();
+            deck = Shuffle(deck);
 
             foreach(Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
             Console.WriteLine(deck.Cards.Count);
-            ////Console.WriteLine(cardOne.Face + " of " + cardOne.Suit);
             Console.Read(); 
 
+        }
 
+        public static Deck Shuffle(Deck deck)
+        {
+            List<Card> TempList = new List<Card>();
+            Random random = new Random();
 
-
+            while(deck.Cards.Count > 0)
+            {
+                int num = random.Next(0, deck.Cards.Count);
+                TempList.Add(deck.Cards[num]);
+                deck.Cards.RemoveAt(num);
+            }
+            deck.Cards = TempList;
+            return deck;
         }
     }
 
