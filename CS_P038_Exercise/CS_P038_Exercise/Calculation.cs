791df1c2e52f1bdc912f81919035d91342b934d7
Yohei <yo_kubocchi@hotmail.com>
2018-03-29 19:26:10 -0700

    New exercise

---
+++ CS_P038_Exercise/CS_P038_Exercise/Calculation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_P038_Exercise
+{
+    public class Calculation
+    {
+        public int tempF { get; set; }
+        public decimal usD { get; set; }
+        public string num { get; set; }
+        public List<string> topics { get; set; }
+        public List<string> scales { get; set; }
+
+        public Calculation()
+        {
+            this.topics = new List<string>();
+            topics.Add("Temperature");
+            topics.Add("Money");
+            topics.Add("Pizza");
+
+            this.scales = new List<string>();
+            scales.Add("degrees Celcius");
+            scales.Add("japanese yen");
+            scales.Add("whole pies of Pizza");
+
+        }
+
+
+        // Temperature converter from Fahrenheit(F) to Celsius(C)
+        public int Status(int tempF)
+        {
+            int tempC = (tempF - 32) * 5 / 9; 
+
+            return tempC;
+        }
+
+        // Calculation to exchange USD to JPY
+        public int Status(decimal usD)
+        {
+            // USD1.00 = JPY116.90 @3/29/2018
+            decimal exRate = 116.90m;
+            int jpY = Convert.ToInt32(usD * exRate);
+
+            return jpY;
+        }
+        
+        // Calculate how many whole pizza you will need for today's party
+        public int Status(string num)
+        {
+            // One whole pizza will be devided to 12 slices, and each 
+            //person would eat 4 slices as assumption 
+            decimal temp = (Convert.ToDecimal(num) * 4) / 12;
+
+            int numPizza = Convert.ToInt32(Math.Ceiling(temp)); 
+
+            return numPizza;
+        }
+
+    }
+}
---
+++ CS_P038_Exercise/CS_P038_Exercise/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_P038_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation calculation = new Calculation();
+
+            Console.WriteLine("Select a topic from below and type a number: " +
+                "\n1: Temperature, 2: Money, 3: Today's party.");
+
+            try
+            {
+                int topic = Convert.ToInt16(Console.ReadLine());
+                bool vali = false;
+                int result = 0;
+            
+
+                while (!vali)
+                {
+                    switch (topic)
+                    {
+                        case 1:
+                            Console.WriteLine("What's the temperature in Fahrenheit? This is converting to Celcius. Do not input fraction.");
+                            // passing a integer value 
+                            result = calculation.Status(Convert.ToInt16(Console.ReadLine()));
+                            vali = true;
+                            break;
+
+                        case 2:
+                            Console.WriteLine("How many dollers in your wallet? This is converting japanese yen.");
+                            //passing a Decimal value
+                            result = calculation.Status(Convert.ToDecimal(Console.ReadLine()));
+                            vali = true;
+                            break;
+
+                        case 3:
+                            Console.WriteLine("You are having a party today. How many friends coming? \nThis is calculating how many whole pies of Pizza you will need." +
+                                "\nAssumption:Each whole pizza is devided to 12 slices. Each person is supposed to eat 3 slices of pizza.");
+                            // passing a string value
+                            result = calculation.Status(Console.ReadLine());
+                            vali = true;
+                            break;
+
+                        default:
+                            Console.WriteLine("Something is wrong. Type again.\n Topic number?");
+                            topic = Convert.ToInt16(Console.ReadLine());
+                            break;
+                    }
+                } // End while
+
+            Console.WriteLine("You chose " + calculation.topics[topic - 1] 
+                + "\nThe answer is :" + result.ToString("N0") + " " + calculation.scales[topic - 1] );
+            Console.ReadLine();
+
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.Read();
+            }
+        }
+    }
+}
