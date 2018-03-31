cd0b2192ddc53601e6bdd894565fcb1fef599ef2
Yohei <yo_kubocchi@hotmail.com>
2018-03-30 18:40:53 -0700

    New Exercise

---
+++ CS_P040_Exercise/CS_P040_Exercise/Deal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_P040_Exercise
+{
+    class Deal
+    {
+        public void Power(int num1, int num2)
+        {
+             int result = num1;
+
+            for(int i=0; i<num2; i++)
+            {
+                result = result * num1;
+            }
+
+            Console.WriteLine("You inputed {0} and {1}. Then this multiplied " +
+                "{0} to itself {1} times.\nResult: {2:N0}",
+                num1, num2, result);
+        }
+    }
+}
---
+++ CS_P040_Exercise/CS_P040_Exercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_P040_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deal deal = new Deal();
+
+            Console.Write("Type 2 numbers.\nNumber 1: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Number 2: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            deal.Power(num1, num2);
+            Console.ReadLine();
+        }
+    }
+}
