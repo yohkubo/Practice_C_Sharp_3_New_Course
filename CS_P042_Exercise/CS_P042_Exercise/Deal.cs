0c54e59f355af8c5766a7872be29426fd33f57fc
Yohei <yo_kubocchi@hotmail.com>
2018-04-01 14:29:16 -0700

    new exercise

---
+++ CS_P042_Exercise/CS_P042_Exercise/Deal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_P042_Exercise
+{
+    public static class  Deal
+    {
+
+        public static void Divide(int num, out int result)
+        {
+            result = num / 2;
+        }
+
+        public static void Divide(int num, int times, out int result)
+        {
+            result = num / Convert.ToInt16(Math.Pow(2, Convert.ToDouble(times)));
+        }
+        
+    }
+}
--- CS_P042_Exercise/CS_P042_Exercise/Program.cs
+++ CS_P042_Exercise/CS_P042_Exercise/Program.cs
@@ -11,6 +11,32 @@ namespace CS_P042_Exercise
         static void Main(string[] args)
         {
 
+            int intResult;
+
+            Console.WriteLine("Type 2 numbers. Second number is optional.\n1st No.: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Deal.Divide(num1, out intResult);
+
+            int num2 = 0; //Optional number
+            Console.WriteLine("2nd No.: ");
+
+            // Running when second number inputed
+            if (int.TryParse(Console.ReadLine(), out num2))
+            {
+                Deal.Divide(num1, num2, out intResult);
+                Console.WriteLine("You typed {0} and {1}. Then this divided {0} by 2 for {1} times.\nResult is: {2}",
+                    num1, num2, intResult);
+            }
+
+            else
+            {
+            Console.WriteLine("You typed {0}, divided by 2\nResult: {1}",
+                num1, intResult);
+            }
+
+            Console.WriteLine("Results are rounded down to one significant figures every time divided.");
+            Console.ReadLine();
+            
         }
     }
 }
