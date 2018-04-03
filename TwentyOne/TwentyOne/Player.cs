fbd2189d056ee06d95a33a9ff3fe99bab20c8f6f
Yohei <yo_kubocchi@hotmail.com>
2018-04-03 14:10:50 -0700

    Added Player class

---
+++ TwentyOne/TwentyOne/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class Player
+    {
+        public List<Card> Hand { get; set; }
+        public int Balance { get; set; }
+        public string Name { get; set; }
+        public bool isActivelyPlaying { get; set; }
+
+
+    }
+}
