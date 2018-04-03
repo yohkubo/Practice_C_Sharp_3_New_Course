3004bd44482d743c3e408ec75c6313e43da5e364
Yohei <yo_kubocchi@hotmail.com>
2018-04-02 17:39:05 -0700

    New exercise
    Polymorphism, abstract class

---
+++ CS_P048_Exercise/CS_P048_Exercise/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_P048_Exercise
+{
+    class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Employee's Name: {0} {1}", FirstName, LastName);
+        }
+    }
+}
