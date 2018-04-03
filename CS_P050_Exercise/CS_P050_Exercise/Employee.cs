3eb857e769764fac7a6f1131924c8bdabfee4d3f
Yohei <yo_kubocchi@hotmail.com>
2018-04-02 20:09:30 -0700

    New exercise for Interface

---
+++ CS_P050_Exercise/CS_P050_Exercise/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_P050_Exercise
+{
+    class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Employee's Name: {0} {1}", FirstName, LastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("You have quit the job...");             
+        }
+    }
+}
---
+++ CS_P050_Exercise/CS_P050_Exercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_P050_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable quittable = new Employee() {FirstName = "Yohei", LastName = "Kubo" };
+            
+            quittable.Quit();
+            Console.ReadLine();
+
+
+            // For test
+            //Employee employee = new Employee() { FirstName="Ken", LastName="Saga" };
+            //employee.SayName();
+            //employee.Quit();
+            //Console.ReadLine();
+            
+        }
+    }
+}
