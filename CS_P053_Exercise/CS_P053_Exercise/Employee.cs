c5333921001bd6db3f2495d1916dbc5f6871ec3d
Yohei <yo_kubocchi@hotmail.com>
2018-04-04 16:36:03 -0700

    New Exercise

---
+++ CS_P053_Exercise/CS_P053_Exercise/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_P053_Exercise
+{
+    public class Employee : Person, IQuittable
+    {
+        public int Id { get; set; }
+
+        public override void SayName()
+        {
+            Console.WriteLine("Employee's Name: {0} {1}", FirstName, LastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("{0} {1}, You have quit the job...", FirstName, LastName);
+        }
+
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            return employee1.Id == employee2.Id;
+        }
+
+        public static bool operator!= (Employee employee1, Employee employee2)
+        {       
+            return !ReferenceEquals(employee1.Id, employee2.Id);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+    }
+}
---
+++ CS_P053_Exercise/CS_P053_Exercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_P053_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee()
+            { FirstName = "Yohei", LastName = "Kubo", Id = 1 };
+            Employee employee2 = new Employee()
+            { FirstName = "Ken", LastName = "Saga", Id = 1 };
+
+            if (employee1 == employee2)
+                Console.WriteLine("{0} and {1} have same ID '{2}'.", 
+                    employee1.LastName, employee2.LastName, employee1.Id);
+            else Console.WriteLine("{0} is ID:{1}. {2} is ID:{3}.", 
+                employee1.LastName, employee1.Id, employee2.LastName, employee2.Id);
+
+            Console.ReadLine();
+        }
+    }
+}
